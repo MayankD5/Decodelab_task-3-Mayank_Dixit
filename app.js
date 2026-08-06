c7b2594d49f4436dc18176f85d26ac52598aada1
MayankD5 <mayankd9497@gmail.com>
2026-08-06 17:03:30 +0530

    Finalize Student Management REST API with MySQL

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require("express");
 require("./config/db");
 
-const app = express();
-
-app.use(express.json());
-
 const studentRoutes = require("./routes/students");
+
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(express.json());
 
 // Home Route
 app.get("/", (req, res) => {
-    res.send("Welcome to Student REST API");
+    res.send("Welcome to the Student Management REST API with MySQL");
 });
 
 // Student Routes
 app.use("/students", studentRoutes);
 
-const PORT = 3000;
-
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
