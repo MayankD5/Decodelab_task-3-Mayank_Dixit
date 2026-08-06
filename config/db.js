ae4045d99092bfe1d49fdcaff30f2fbbe7f22297
MayankD5 <mayankd9497@gmail.com>
2026-08-06 17:13:58 +0530

    Update database configuration

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "YOUR_PASSWORD",
+    password: "PASSword",
     database: "student_management"
 });
 
